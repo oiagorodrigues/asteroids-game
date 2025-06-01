ec480bfb7b3aa1adc41e508a0ff3b1b9dc69cffb
Iago Rodrigues <yagomello16@gmail.com>
2025-06-01 17:45:30 -0300

    chore: add updatable and drawable container group

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     """ delta time: represents the amount of time that has passed since the last frame was drawn"""
@@ -19,10 +25,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for group in drawable:
+            group.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
--- player.py
+++ player.py
@@ -6,6 +6,8 @@ from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
+    containers: tuple[pygame.sprite.Group] = ()
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
